249ccab7f7ed37d37e6a2b8b193e7b22aceddeb3
jobell <jbakwd@163.com>
2016-06-24 11:44:01 +0800

    添加CacheChannel类，完成统一的缓存处理操作

---
+++ atoms-core/src/main/java/com/lovver/atoms/core/CacheChannel.java
@@ -0,0 +1,136 @@
+package com.lovver.atoms.core;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+import com.lovver.atoms.cache.Cache;
+import com.lovver.atoms.cache.CacheProvider;
+import com.lovver.atoms.common.exception.CacheException;
+import com.lovver.atoms.config.AtomsCacheBean;
+import com.lovver.atoms.config.AtomsConfig;
+import com.lovver.atoms.context.AtomsContext;
+
+public class CacheChannel {
+	private static AtomsCacheBean level1CacheBean=AtomsContext.getAtomsCacheBean(1) ;
+
+	/**
+	 * 获取缓存中的数据
+	 * 
+	 * @param region
+	 *            : Cache Region name
+	 * @param key
+	 *            : Cache key
+	 * @return cache object
+	 */
+	public Object get(String region, Object key) {
+		
+		if (region != null && key != null) {
+			Cache tCache=CacheManager.getCache(1, region, true);
+			Object value=tCache.get(key);
+			if(value==null){
+				String expiredOp=level1CacheBean.getExpiredOperator();
+				if("update".equals(expiredOp.toLowerCase())){
+					Map<String,CacheProvider> mCacheProvider=AtomsContext.getCacheProvider();
+					for(int i=2;i<=mCacheProvider.size();i++){
+						
+						Cache cache=AtomsContext.getCache(region, i);
+						value=cache.get(key);
+						if(value!=null){
+							break;
+						}
+					}
+					Cache cache=CacheManager.getCache(1,region,true);
+					cache.put(key, value); 
+				}
+			}
+			return value;
+		}
+		return null;
+	}
+
+	/**
+	 * 写入缓存
+	 * 
+	 * @param region
+	 *            : Cache Region name
+	 * @param key
+	 *            : Cache key
+	 * @param value
+	 *            : Cache value
+	 */
+	public void set(String region, Object key, Object value) {
+		if (region != null && key != null) {
+			if (value == null)
+				evict(region, key);
+			else {
+				Cache cache=CacheManager.getCache(1,region,true);
+				cache.put(key, value); 
+			}
+		}
+	}
+
+	/**
+	 * 删除缓存
+	 * 
+	 * @param region
+	 *            : Cache Region name
+	 * @param key
+	 *            : Cache key
+	 */
+	public void evict(String region, Object key) {
+		Cache cache=CacheManager.getCache(1,region,true);
+		cache.evict(key); 
+	}
+
+	/**
+	 * 批量删除缓存
+	 * 
+	 * @param region
+	 *            : Cache region name
+	 * @param keys
+	 *            : Cache key
+	 */
+	@SuppressWarnings({ "rawtypes" })
+	public void batchEvict(String region, List keys) {
+		Cache cache=CacheManager.getCache(1,region,true);
+		cache.evict(keys); 
+	}
+
+	/**
+	 * Clear the cache
+	 * 
+	 * @param region
+	 *            : Cache region name
+	 */
+	public void clear(String region) throws CacheException {
+		Cache cache=CacheManager.getCache(1,region,true);
+		cache.clear(); 
+	}
+
+	/**
+	 * Get cache region keys
+	 * 
+	 * @param region
+	 *            : Cache region name
+	 * @return key list
+	 */
+	@SuppressWarnings("rawtypes")
+	public List keys(String region) throws CacheException {
+		Cache cache=CacheManager.getCache(1,region,true);
+		return cache.keys();
+	}
+
+	
+
+	/**
+	 * 关闭到通道的连接
+	 */
+	public void close() {
+		Collection<AtomsCacheBean> lstCacheBean=AtomsConfig.getAtomsConfig().getCache();
+		for(AtomsCacheBean cp:lstCacheBean){
+			CacheManager.shutdown(Integer.parseInt(cp.getLevel()));
+		}
+		
+	}
+}
--- atoms-core/src/main/java/com/lovver/atoms/core/CacheManager.java
+++ atoms-core/src/main/java/com/lovver/atoms/core/CacheManager.java
@@ -1,11 +1,13 @@
 package com.lovver.atoms.core;
 
+import java.util.List;
 import java.util.Map;
 
 import com.lovver.atoms.cache.Cache;
 import com.lovver.atoms.cache.CacheEventListener;
 import com.lovver.atoms.cache.CacheEventListenerFactory;
 import com.lovver.atoms.cache.CacheProvider;
+import com.lovver.atoms.common.exception.CacheException;
 import com.lovver.atoms.context.AtomsContext;
 
 public class CacheManager {
@@ -21,5 +23,119 @@ public class CacheManager {
 		CacheEventListener listener = null;
 		listener = CacheEventListenerFactory.getCacheEventListener(cacheProvider.name(),level+"");
 		return cacheProvider.buildCache(regionName, autoCreate, listener);
+	}
+	
+	
+	public static void shutdown(int level){
+		CacheProvider cacheProvider=cacheProviders.get(level+"");
+		cacheProvider.stop();
+	}
+	
+	
+	
+	
+	/**
+	 * 获取缓存中的数据
+	 * @param level Cache Level: L1 and L2
+	 * @param name Cache region name
+	 * @param key Cache key
+	 * @return Cache object
+	 */
+	public final static Object get(int level, String name, Object key){
+		//System.out.println("GET1 => " + name+":"+key);
+		if(name!=null && key != null) {
+            Cache cache = getCache(level, name, false);
+            if (cache != null)
+                return cache.get(key);
+        }
+		return null;
+	}
+	
+	/**
+	 * 获取缓存中的数据
+	 * @param level Cache Level -&gt; L1 and L2
+	 * @param resultClass Cache object class
+	 * @param name Cache region name
+	 * @param key Cache key
+	 * @return Cache object
+	 */
+	@SuppressWarnings("unchecked")
+	public final static <T> T get(int level, Class<T> resultClass, String name, Object key){
+		//System.out.println("GET2 => " + name+":"+key);
+		if(name!=null && key != null) {
+            Cache cache =getCache(level, name, false);
+            if (cache != null)
+                return (T)cache.get(key);
+        }
+		return null;
+	}
+	
+	/**
+	 * 写入缓存
+	 * @param level Cache Level: L1 and L2
+	 * @param name Cache region name
+	 * @param key Cache key
+	 * @param value Cache value
+	 */
+	public final static void set(int level, String name, Object key, Object value){
+		//System.out.println("SET => " + name+":"+key+"="+value);
+		if(name!=null && key != null && value!=null) {
+            Cache cache =getCache(level, name, true);
+            if (cache != null)
+                cache.put(key,value);
+        }
+	}
+	
+	/**
+	 * 清除缓存中的某个数据
+	 * @param level Cache Level: L1 and L2
+	 * @param name Cache region name
+	 * @param key Cache key
+	 */
+	public final static void evict(int level, String name, Object key){
+		//batchEvict(level, name, java.util.Arrays.asList(key));
+		if(name!=null && key != null) {
+            Cache cache =getCache(level, name, false);
+            if (cache != null)
+                cache.evict(key);
+        }
+	}
+	
+	/**
+	 * 批量删除缓存中的一些数据
+	 * @param level Cache Level： L1 and L2
+	 * @param name Cache region name
+	 * @param keys Cache keys
+	 */
+	@SuppressWarnings("rawtypes")
+	public final static void batchEvict(int level, String name, List keys) {
+		if(name!=null && keys != null && keys.size() > 0) {
+            Cache cache =getCache(level, name, false);
+            if (cache != null)
+                cache.evict(keys);
+        }
+	}
+
+	/**
+	 * Clear the cache
+	 * @param level Cache level
+	 * @param name cache region name
+	 */
+	public final static void clear(int level, String name) throws CacheException {
+        Cache cache =getCache(level, name, false);
+        if(cache != null)
+        	cache.clear();
+	}
+	
+	/**
+	 * list cache keys
+	 * @param level Cache level
+	 * @param name cache region name
+	 * @return Key List
+	 */
+	@SuppressWarnings("rawtypes")
+	public final static List keys(int level, String name) throws CacheException {
+        Cache cache =getCache(level, name, false);
+		return (cache!=null)?cache.keys():null;
 	}
 }
