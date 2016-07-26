532e191ca79fd1243b4534a40852861db36f15ed
jobell <jbakwd@163.com>
2016-07-26 17:03:20 +0800

    修改打包遗漏配置文件的问题

--- atoms/src/test/java/com/lovver/atoms/CacheChannelTest.java
+++ atoms/src/test/java/com/lovver/atoms/CacheChannelTest.java
@@ -7,14 +7,14 @@ public class CacheChannelTest {
 	public static void main(String[] args) {
 		CacheChannel cc=CacheChannel.getInstance();
 		cc.set("hello", "dddd", "nihaoya");
-		while(true){
-			Object value=cc.get("hello", "dddd");
-			if(value==null){
-				System.out.println("==============="+value);
-			}else{
-				System.out.println("==============="+value);
-			}
-		}
+//		while(true){
+//			Object value=cc.get("hello", "dddd");
+//			if(value==null){
+//				System.out.println("==============="+value);
+//			}else{
+//				System.out.println("==============="+value);
+//			}
+//		}
 //		for(int i=0;i<100000;i++){
 //			System.out.println("==============="+cc.get("jiangbiao", "hello"));
 //		}
