3e4b4e80b48c38202b672d50c9e9439368059fea
jobell <jbakwd@163.com>
2016-10-27 10:18:24 +0800

    通知处理操作添加原子处理。

--- atoms-broadcast/atoms-broadcast-redis/src/main/java/com/lovver/atoms/broadcast/redis/RedisPubSub.java
+++ atoms-broadcast/atoms-broadcast-redis/src/main/java/com/lovver/atoms/broadcast/redis/RedisPubSub.java
@@ -1,5 +1,7 @@
 package com.lovver.atoms.broadcast.redis;
 
+import java.util.Map;
+
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPubSub;
 
@@ -7,9 +9,11 @@ import com.alibaba.fastjson.JSON;
 import com.lovver.atoms.broadcast.BroadCast;
 import com.lovver.atoms.broadcast.Command;
 import com.lovver.atoms.cache.Cache;
+import com.lovver.atoms.cache.CacheProvider;
 import com.lovver.atoms.common.utils.StringUtils;
 import com.lovver.atoms.config.AtomsBroadCastBean;
 import com.lovver.atoms.config.AtomsBroadCastConfigBean;
+import com.lovver.atoms.config.AtomsCacheBean;
 import com.lovver.atoms.context.AtomsContext;
 import com.lovver.atoms.serializer.Serializer;
 
@@ -50,6 +54,34 @@ public class RedisPubSub extends JedisPubSub{
 		jedis.close();
 	}
 	
+	private static byte getExpiredOperator(){
+		String expiredOperator;
+		AtomsCacheBean cacheBean=AtomsContext.getAtomsCacheBean(1);
+		expiredOperator=cacheBean.getExpiredOperator();
+		if(!StringUtils.isEmpty(expiredOperator)){
+			expiredOperator=expiredOperator.toLowerCase();
+		}
+		if("update".equals(expiredOperator)){
+			return Command.EXPIRE_UPDATE;
+		}else{
+			return Command.EXPIRE_DELETE;
+		}
+	}
+	
+	private static boolean getDeleteAtom(){
+		String delete_atom;
+		AtomsCacheBean cacheBean=AtomsContext.getAtomsCacheBean(1);
+		delete_atom=cacheBean.getDelete_atom();
+		if(!StringUtils.isEmpty(delete_atom)){
+			delete_atom=delete_atom.toLowerCase();
+		}
+		if("true".equals(delete_atom)){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
 	 public void onMessage(String channel, String message){
 		 System.out.println("onMessage "+ level+"===="+this);
 		if (message != null && message.length() <= 0) {
@@ -66,28 +98,58 @@ public class RedisPubSub extends JedisPubSub{
 			
 			int targetCache=this.level+1;
 			
-			Cache cache=AtomsContext.getCache(cmd.getRegion(), targetCache);
-			BroadCast broadCast=AtomsContext.getBroadCast().get(targetCache+"");
+//			Cache cache=AtomsContext.getCache(cmd.getRegion(), targetCache);
+			BroadCast broadCast=null;
+			boolean delete_atom=getDeleteAtom();
 			switch (cmd.getOperator()) {
 			case Command.OPT_DELETE_KEY:
-				cache.evict(cmd.getKey()); 
-				if(broadCast!=null){
-					Command bcmd=new Command(Command.OPT_DELETE_KEY,cmd.getRegion(),cmd.getKey());
-					broadCast.broadcast(JSON.toJSONString(bcmd));
+				if(true==delete_atom){
+					Map<String,CacheProvider> mCacheProvider=AtomsContext.getCacheProvider();
+					for(int i=targetCache;i<=mCacheProvider.size();i++){
+						
+						Cache cache=AtomsContext.getCache(cmd.getRegion(), i);
+						cache.evict(cmd.getKey()); 
+					}
+				}else{
+//					cache.evict(cmd.getKey()); 
+					broadCast=AtomsContext.getBroadCast().get(targetCache+"");
+					if(broadCast!=null){
+						Command bcmd=new Command(Command.OPT_DELETE_KEY,cmd.getRegion(),cmd.getKey());
+						broadCast.broadcast(JSON.toJSONString(bcmd));
+					}
 				}
 				break;
 			case Command.OPT_CLEAR_KEY:
-				cache.clear();
-				if(broadCast!=null){
-					Command bcmd=new Command(Command.OPT_CLEAR_KEY,cmd.getRegion());
-					broadCast.broadcast(JSON.toJSONString(bcmd));
+				     
+				if(true==delete_atom){
+					Map<String,CacheProvider> mCacheProvider=AtomsContext.getCacheProvider();
+					for(int i=targetCache;i<=mCacheProvider.size();i++){
+						
+						Cache cache=AtomsContext.getCache(cmd.getRegion(), i);
+						cache.clear();
+					}
+				}else{
+					broadCast=AtomsContext.getBroadCast().get(targetCache+"");
+					if(broadCast!=null){
+						Command bcmd=new Command(Command.OPT_CLEAR_KEY,cmd.getRegion());
+						broadCast.broadcast(JSON.toJSONString(bcmd));
+					}
 				}
 				break;
 			case Command.OPT_PUT_KEY:
-				cache.put(cmd.getKey(), serializer.deserialize(cmd.getValue())); 
-				if(broadCast!=null){
-					Command bcmd=new Command(Command.OPT_PUT_KEY,cmd.getRegion(),cmd.getKey(),cmd.getValue());
-					broadCast.broadcast(JSON.toJSONString(bcmd));
+				if(true==delete_atom){
+					Map<String,CacheProvider> mCacheProvider=AtomsContext.getCacheProvider();
+					for(int i=targetCache;i<=mCacheProvider.size();i++){
+						
+						Cache cache=AtomsContext.getCache(cmd.getRegion(), i);
+						cache.put(cmd.getKey(), serializer.deserialize(cmd.getValue())); 
+					}
+				}else{
+					broadCast=AtomsContext.getBroadCast().get(targetCache+"");
+					if(broadCast!=null){
+						Command bcmd=new Command(Command.OPT_PUT_KEY,cmd.getRegion(),cmd.getKey(),cmd.getValue());
+						broadCast.broadcast(JSON.toJSONString(bcmd));
+					}
 				}
 				break;
 			default:
