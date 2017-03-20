9667529eae4ae3073bbeedfee13f00c34be78306
jobell <jobell@aliyun.com>
2017-03-20 15:03:32 +0800

    添加单key失效测试用例

---
+++ atoms/src/test/java/com/lovver/atoms/CacheSingleKeyExpireTest.java
@@ -0,0 +1,26 @@
+package com.lovver.atoms;
+
+import com.lovver.atoms.core.CacheChannel;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+
+/**
+ * Created by Administrator on 2017/3/20.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(value = {"classpath:applicationContext.xml"})
+@TransactionConfiguration(defaultRollback = false)
+public class CacheSingleKeyExpireTest {
+
+    @Test
+    public void run() throws InterruptedException {
+        CacheChannel cc = CacheChannel.getInstance();
+        cc.set("hello", "dddd", "jobell_", 1);
+         Thread.sleep(2000);
+
+        System.out.println("===============" + cc.get("hello", "dddd"));
+    }
+}
